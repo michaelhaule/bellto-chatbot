3fc9c0f3b583909db735845dd467dccc591a7d84
michaelhaule <mikael.haule1@gmail.com>
2021-09-01 01:20:53 +0300

    Changed constant js

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,5 +1,4 @@
-var serverURL = document.getElementById("belltro-chat-widget")
-const rasa_server_url= serverURL.dataset.server_url
+const rasa_server_url= "http://159.138.169.207:80/fundi_webhook"
 const action_name = "action_hello_world";
 const sender_id = uuidv4();
 
